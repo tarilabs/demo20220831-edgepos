380330b10669b086e8d326ec1946f8aee53f73d1
tarilabs <matteo.mortari@gmail.com>
2022-09-05 09:19:23 +0200

    basic test only 1 type of alert reported

---
+++ src/test/java/org/drools/hackfest2022/BasicTest.java
@@ -0,0 +1,44 @@
+package org.drools.hackfest2022;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.drools.hackfest2022.model.Item;
+import org.drools.hackfest2022.model.ItemCategory;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@QuarkusTest
+public class BasicTest {
+
+    static {
+      RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    @Test
+    public void testHelloEndpoint() {
+        Item item1 = new Item("barcode", "orange", ItemCategory.GROCERY, 45, 1);
+        Item item2 = new Item("barcode", "orange", ItemCategory.GROCERY, 45, 1);
+
+        String response = given()
+          .body(Map.of("items", Arrays.asList(item1, item2)))
+          .contentType(ContentType.JSON)
+          .accept(ContentType.JSON)
+          .when().post("/alert")
+          .then()
+          .statusCode(200)
+          .extract().asString();
+
+        System.out.println(response);
+
+        assertTrue(response.contains("Was item weighted correctly at the self-service station?"));
+        assertTrue(response.contains("Please check you haven't scanned this item more than once?"));
+    }
+
+}
--- src/test/java/org/drools/hackfest2022/GreetingResourceIT.java
+++ src/test/java/org/drools/hackfest2022/GreetingResourceIT.java
@@ -3,7 +3,7 @@ package org.drools.hackfest2022;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
+public class GreetingResourceIT extends BasicTest {
 
     // Execute the same tests but in native mode.
 }
