134f843c88e728d2824199bf12258753bebcc542
tarilabs <matteo.mortari@gmail.com>
2022-11-30 17:10:36 +0100

    Prepare for integration with UI

--- src/main/java/org/drools/hackfest2022/GreetingResource.java
+++ src/main/java/org/drools/hackfest2022/GreetingResource.java
@@ -1,16 +1,23 @@
 package org.drools.hackfest2022;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/hello")
+import org.drools.hackfest2022.model.Item;
+import org.drools.hackfest2022.model.ItemCategory;
+
+@Path("/scan")
 public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response hello(String payload) {
+        return Response.ok(new Item("zxing", payload, ItemCategory.GROCERY, 0, 0)).build();
     }
 }
