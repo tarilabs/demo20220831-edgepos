ea9be8085003544726e73740754da282dacc252f
tarilabs <matteo.mortari@gmail.com>
2022-12-06 09:01:59 +0100

    refactor

--- src/main/java/org/drools/hackfest2022/Utils.java
+++ src/main/java/org/drools/hackfest2022/Utils.java
@@ -7,6 +7,11 @@ public class Utils {
     public static Number sum(Number... values) {
         return Stream.of(values).mapToDouble(Number::doubleValue).sum();
     }
+
+    public static double round(Number operand, int decimals) {
+        double y = Math.pow(10.0, decimals);
+        return Math.round(operand.doubleValue() * y) / y;
+    }
     
     private Utils() {
         // only static methods
